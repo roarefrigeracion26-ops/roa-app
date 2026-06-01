6b705c02ac8f03b2e024a9079522ec3c3d5a4418
Alexis Torrado <sgmr2027@gmail.com>
2026-05-31 21:21:24 -0500

    feat: agregar rutas agregar_equipo_antes y completar_equipo_despues

--- operations/urls.py
+++ operations/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('cliente/<int:cliente_id>/orden/nueva/', views.NuevaOrdenClienteView.as_view(), name='nueva_orden_cliente'),
     path('orden/nueva/<int:equipo_id>/', views.NuevaOrdenView.as_view(), name='nueva_orden'),
     path('orden/<int:orden_id>/formulario/', views.FormularioOrdenView.as_view(), name='formulario_orden'),
-    path('orden/<int:orden_id>/equipo/agregar/', views.AgregarEquipoView.as_view(), name='agregar_equipo'),
+    path('orden/<int:orden_id>/equipo/agregar-antes/', views.AgregarEquipoAntesView.as_view(), name='agregar_equipo_antes'),
+    path('orden/<int:orden_id>/equipo/<int:equipo_id>/completar-despues/', views.CompletarEquipoDespuesView.as_view(), name='completar_equipo_despues'),
 
     path('orden/<int:orden_id>/finalizar/', views.FinalizarOrdenView.as_view(), name='finalizar_orden'),
     path('orden/<int:orden_id>/cerrada/', views.OrdenCerradaView.as_view(), name='orden_cerrada'),
